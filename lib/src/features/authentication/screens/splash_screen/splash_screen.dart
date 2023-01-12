6ccefaa4e1298dedc98574a6659e5c3ed7991df9
Vincent Kamemia <kamemia18@yahoo.com>
2023-01-12 19:27:20 +0300

    Fixes

--- lib/src/features/authentication/screens/splash_screen/splash_screen.dart
+++ lib/src/features/authentication/screens/splash_screen/splash_screen.dart
@@ -19,60 +19,61 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Obx( (() => AnimatedPositioned(
-              duration: const Duration(milliseconds: 1600),
-              top: splashController.animate.value ? 0: -30,
-              left: splashController.animate.value ? 0: -30,
-              child: Image(image: AssetImage(tSplashTopIcon)),
-            )
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 1600),
+            top: splashController.animate.value ? 0: -30,
+            left: splashController.animate.value ? 0: -30,
+            child: Image(
+              image: AssetImage(tSplashTopIcon),
+            ),
           ),
-          Obx(
-            () => AnimatedPositioned(
-              duration: const Duration(milliseconds: 2000),
-              top: 80,
-              left: splashController.animate.value ? tDefaultSize : -80,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 2000),
-                opacity: splashController.animate.value ? 1 : 0,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(tAppName, style: Theme.of(context).textTheme.headline3),
-                    Text(tAppTagLine, style: Theme.of(context).textTheme.headline2)
-                  ],
-                ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 1600),
+            top: 80,
+            left: splashController.animate.value ? tDefaultSize : -80,
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 1600),
+              opacity: splashController.animate.value ? 1 : 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tAppName,
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                  Text(
+                    tAppTagLine,
+                    style: Theme.of(context).textTheme.headline2,
+                  )
+                ],
               ),
             ),
           ),
-          Obx(
-            () =>  AnimatedPositioned(
-              duration: const Duration(milliseconds: 2400),
-              bottom: splashController.animate.value ? 100 : 0,
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 2400),
+            bottom: splashController.animate.value ? 100 : 0,
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 2000),
+              opacity: splashController.animate.value ? 1 : 0,
+              child: Image(image: AssetImage(tSplashImage)),
+              ),
+          ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 2400),
+              bottom: splashController.animate.value ? 60 : 0,
+              right: tDefaultSize,
               child: AnimatedOpacity(
                 duration: const Duration(milliseconds: 2000),
                 opacity: splashController.animate.value ? 1 : 0,
-                child: Image(image: AssetImage(tSplashImage)),
-                ),
-            ),
-          ),
-          Obx(
-            () => AnimatedPositioned(
-              duration: const Duration(milliseconds: 2400),
-                bottom: splashController.animate.value ? 60 : 0,
-                right: tDefaultSize,
-                child: AnimatedOpacity(
-                  duration: const Duration(milliseconds: 2000),
-                  opacity: splashController.animate.value ? 1 : 0,
-                  child: Container(
-                    width: tSplashContainerSize,
-                    height: tSplashContainerSize,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: tPrimaryColor,
-                    ),
+                child: Container(
+                  width: tSplashContainerSize,
+                  height: tSplashContainerSize,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: tPrimaryColor,
                   ),
-                )),
-          ),
+                ),
+              )),
         ],
       ),
     );
