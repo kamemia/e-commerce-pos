7875c72c41a2d78c5a085f36bc43a78f65b28a29
Vincent Kamemia <kamemia18@yahoo.com>
2023-01-12 20:08:01 +0300

    Added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:commerce/src/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   runApp(const MyApp());
