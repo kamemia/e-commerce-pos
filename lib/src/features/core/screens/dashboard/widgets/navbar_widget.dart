697a897de877209f1b883d56edf855e3890b6c2c
Vincent Kamemia <kamemia18@yahoo.com>
2023-01-16 08:23:02 +0300

    dashboard UI complete

---
+++ lib/src/features/core/screens/dashboard/widgets/navbar_widget.dart
@@ -0,0 +1,38 @@
+import 'package:commerce/src/constants/image_strings.dart';
+import 'package:flutter/material.dart';
+
+
+class NavbarWidget extends StatefulWidget {
+  const NavbarWidget({super.key});
+
+  @override
+  State<NavbarWidget> createState() => _NavbarWidgetState();
+}
+
+class _NavbarWidgetState extends State<NavbarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green[700],
+      width: double.infinity,
+      height: 200,
+      padding: const EdgeInsets.only(top: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            height: 70,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(tUserProfielImage),
+              )
+            ),
+          ),
+          Text("Welcome to our site")
+        ],
+      ),
+    );
+  }
+}
