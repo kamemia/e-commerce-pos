44dc8d442963b350df16a7bf102471a705c0bc26
Vincent Kamemia <kamemia18@yahoo.com>
2023-02-03 13:20:07 +0300

    Connect to a php backend

---
+++ lib/src/api_connection/api_connection.dart
@@ -0,0 +1,7 @@
+class API {
+  static const hostConnect = "http://192.168.100.11/e-pos_backend";
+  static const hostConnectUser = "$hostConnect/user";
+
+  // Signup user
+  static const signUp = "$hostConnect/user/signup.php";
+}
